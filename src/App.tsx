f151f7889a7ed8954c94e2bcc220640fcb762ade
Or Lavy <orlavyil@gmail.com>
2020-07-13 15:15:53 +0300

    Adds logic for 'app content'

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./App.css";
 import { ContentContainer } from "./components/structure/ContentContainer";
 import { Route, Switch } from "react-router-dom";
@@ -12,30 +12,32 @@ import { Header } from "./components/structure/Header";
 const App = observer(() => {
   const cryptoWalletIntegrationStore = useCryptoWalletIntegrationStore();
 
-  if (!cryptoWalletIntegrationStore.isConnectedToWallet) {
-    return (
-      <main className="App">
-        <ContentContainer id={"appContainer"}>
-          <NoEthereumProviderSection
-            walletConnectionPhase={"connect"}
-            actionFunction={() => cryptoWalletIntegrationStore.askToConnect()}
-          />
-        </ContentContainer>
-      </main>
-    );
-  }
+  const isConnected = cryptoWalletIntegrationStore.isConnectedToWallet;
 
-  return (
-    <main className="App">
-      <Background prismVersion={"0.5"} />
-      <Header />
-      <ContentContainer id={"appContainer"}>
+  const appContent = useMemo(() => {
+    if (!isConnected) {
+      return (
+        <NoEthereumProviderSection
+          walletConnectionPhase={"connect"}
+          actionFunction={() => cryptoWalletIntegrationStore.askToConnect()}
+        />
+      );
+    } else {
+      return (
         <Switch>
           <Route path={"/"}>
             <GuardiansRegisterOrEditPage />
           </Route>
         </Switch>
-      </ContentContainer>
+      );
+    }
+  }, [cryptoWalletIntegrationStore, isConnected]);
+
+  return (
+    <main className="App">
+      <Background prismVersion={"0.5"} />
+      <Header />
+      <ContentContainer id={"appContainer"}>{appContent}</ContentContainer>
     </main>
   );
 });
