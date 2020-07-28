e9e53fdedd49a6f3ce848fcb6e8eb841e5717710
Or Lavy <orlavyil@gmail.com>
2020-07-28 13:47:25 +0300

    Improves display of helper text in form

---
+++ src/components/forms/FormHelperListTexts.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Color from "color";
+
+interface IProps {
+  helperTexts: string[];
+}
+
+const useStyles = makeStyles((theme) => ({
+  helperText: {
+    color: theme.palette.secondary.main,
+  },
+  textsList: {
+    listStylePosition: "outside",
+    paddingLeft: "1rem",
+  },
+}));
+
+export const FormHelperListTexts = React.memo<IProps>((props) => {
+  const classes = useStyles();
+  const { helperTexts } = props;
+  const lastIndex = helperTexts.length - 1;
+  // DEV_NOTE : O.L : Basically we are not supposed to put a 'ul' inside the helper text's 'p'.  It will
+  //                  just show warnings on dev.
+  // TODO : O.L : FUTURE : Fix this, maybe override the 'p' component to something else.
+  return (
+    <ul className={classes.textsList}>
+      {helperTexts.map((helperText, index) => (
+        <React.Fragment key={`fragment_${helperText}`}>
+          <li
+            id={index.toString()}
+            key={helperText}
+            className={classes.helperText}
+          >
+            <Typography style={{ wordWrap: "break-word" }} variant={"caption"}>
+              {helperText}
+            </Typography>
+          </li>
+          {/*{index !== lastIndex && <br key={`br_${helperText}`} />}*/}
+        </React.Fragment>
+      ))}
+    </ul>
+  );
+});
