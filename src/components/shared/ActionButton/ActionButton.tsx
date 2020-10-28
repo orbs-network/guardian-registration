389dd00b07e2a7abed8ab683cc1ce546d548422c
Or Lavy <orlavyil@gmail.com>
2020-10-28 17:37:24 +0200

    Adds action button

---
+++ src/components/shared/ActionButton/ActionButton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Button, ButtonProps } from "@material-ui/core";
+
+interface IProps {}
+
+const useStyles = makeStyles((theme) => ({
+  actionButton: {
+    color: theme.palette.secondary.light,
+    borderColor: theme.palette.secondary.main,
+    transition: "background-color 0.4s linear, color 0.2s linear",
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.light,
+      color: theme.palette.getContrastText(theme.palette.secondary.light),
+      borderColor: theme.palette.getContrastText(theme.palette.secondary.light),
+    },
+  },
+}));
+
+export const ActionButton = React.memo<IProps & ButtonProps>((props) => {
+  const { children, fullWidth, ...rest } = props;
+  const classes = useStyles();
+
+  return (
+    <Button
+      className={classes.actionButton}
+      variant={"outlined"}
+      fullWidth={fullWidth === undefined ? true : fullWidth}
+      type={"submit"}
+      {...rest}
+    >
+      {children}
+    </Button>
+  );
+});
